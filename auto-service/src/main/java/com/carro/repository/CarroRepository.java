91b9d39d1892c06a1e9377026161344ea3627c50
Carlos <guerra_5g11@hotmail.com>
2024-02-15 15:44:06 -0600

    Rest template - feign client

---
+++ auto-service/src/main/java/com/carro/repository/CarroRepository.java
@@ -0,0 +1,16 @@
+package com.carro.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.carro.entity.Carro;
+
+@Repository
+public interface CarroRepository extends JpaRepository<Carro, Integer>{
+	
+	List<Carro> findByUsuarioId(int usuarioId);
+	
+
+}
