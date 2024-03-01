df9171d4f7fd2ccd2e3ecf48c215268ae9329e3e
Carlos <guerra_5g11@hotmail.com>
2024-03-01 16:46:08 -0600

    eureka

--- auto-service/src/main/java/com/carro/AutoServiceApplication.java
+++ auto-service/src/main/java/com/carro/AutoServiceApplication.java
@@ -2,8 +2,10 @@ package com.carro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AutoServiceApplication {
 
 	public static void main(String[] args) {
